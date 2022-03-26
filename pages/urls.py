da12915258231b9f53e87e1c046013afd8f8a6ba
abidkhan03 <khansahb5566@gmail.com>
2022-03-26 13:14:14 +0500

    tourist Guide and and operator form integration

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('touristguideform', views.touristguideform, name = 'touristguideform')
+    # path('about/', views.about, name = 'about'),
+    path('touristGuide-form/', views.touristGuideForm, name = 'touristGuide-form'),
+    path('hotel-form/', views.hotelForm, name = 'hotelForm'),
+    path('tourOperator-form/', views.tourOperatorForm, name = 'tourOperator-form')
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,16 +4,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-
     return render(request, 'pages/index.html')
 
-def touristguideform(request):
-    
-    return render(request, 'pages/touristGuide-form.html')
 
 def touristGuideForm(request):
     return render(request, 'pages/touristGuide-form.html')
 
 def hotelForm(request):
     return render(request, 'pages/hotel-form.html')
+
+
+def tourOperatorForm(request):
+    return render(request, 'pages/tourOperator-form.html')
+
 
