a86021d1024bad5f1c9f197564ff723def4b94f5
Habib Ur Rehman <habib.rehman0088@gmail.com>
2022-03-30 15:46:32 +0500

    Integrate form

---
+++ travel/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class travelagency(models.Model):
+    business_name = models.CharField(max_length=200)
+    business_foreign_name = models.CharField(max_length=200)
+    nature_of_firm = models.CharField(max_length=200)
+    business_name = models.CharField(max_length=200)
+    business_name = models.CharField(max_length=200)
+    business_name = models.CharField(max_length=200)
