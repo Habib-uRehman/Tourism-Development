f9f08b041357f759ba9409a043a25b487d8744c4
shahzoorkhan <lamborghinia693@gmail.com>
2022-03-31 17:25:07 +0500

    creating models for restraunt

--- tourism/settings.py
+++ tourism/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'restraunt.apps.RestrauntConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
