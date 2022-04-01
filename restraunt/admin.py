2cd42821db376c20479cbc6cd250e4d45adf28a3
shahzoorkhan <lamborghinia693@gmail.com>
2022-04-01 18:06:51 +0500

    restraunt models 80% complete

--- restraunt/admin.py
+++ restraunt/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from . models import restraunt , ownership , manager
 # Register your models here.
+admin.site.rigister(restraunt)
+admin.site.rigister(ownership)
+admin.site.rigister(manager)
+
