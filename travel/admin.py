5b3157082b42883a59726a3e8827973e480c3bd1
Habib Ur Rehman <habib.rehman0088@gmail.com>
2022-04-01 15:55:23 +0500

    Travel Agency Model Created

--- travel/admin.py
+++ travel/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import travelagency, assets, partner, manager, director
+
+admin.site.register(travelagency)
+admin.site.register(assets)
+admin.site.register(partner)
+admin.site.register(manager)
+admin.site.register(director)
+
+
