091614fd30f3244df61a2a9b4a2f3de529e447be
shahzoorkhan <lamborghinia693@gmail.com>
2022-04-03 23:27:29 +0500

    admin panel customization

--- tourism/urls.py
+++ tourism/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('jet/', include('jet.urls', 'jet')),
+    path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     path('', include('pages.urls')),
     path('', include('restraunt.urls')),
     path('admin/', admin.site.urls)
