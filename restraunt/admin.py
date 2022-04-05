7596ad9577ff744978e6cde1e26e3e3c8ceb938a
shahzoorkhan <lamborghinia693@gmail.com>
2022-04-05 22:34:29 +0500

    dynamic models progress 20%

--- restraunt/admin.py
+++ restraunt/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from . models import restraunt , ownership , manager ,furniture_dining , furniture_kitchen
+from .models import Author, Book
 # Register your models here.
 admin.site.register(restraunt)
 admin.site.register(ownership)
@@ -7,3 +8,13 @@ admin.site.register(manager)
 admin.site.register(furniture_dining)
 admin.site.register(furniture_kitchen)
 
+
+class BookInLineAdmin(admin.TabularInline):
+    model = Book
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    inlines = [BookInLineAdmin]
+
+
+admin.site.register(Author, AuthorAdmin)
--- restraunt/models.py
+++ restraunt/models.py
@@ -175,3 +175,18 @@ class manager(models.Model):
     manager_telephone = models.IntegerField(null=True)
 
 
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    number_of_pages = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.title
