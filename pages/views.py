bae7bdd0df08c6e8bfd2bbdffb206c16a86ebdcd
abidkhan03 <khansahb5566@gmail.com>
2022-04-06 13:12:22 +0500

    hotel registration model work done

--- pages/views.py
+++ pages/views.py
@@ -7,8 +7,8 @@ def index(request):
     return render(request, 'pages/index.html')
 
 
-def touristGuideForm(request):
-    return render(request, 'pages/touristGuide-form.html')
+# def touristGuideForm(request):
+#     return render(request, 'pages/touristGuide-form.html')
 
 # def hotelForm(request):
 #     return render(request, 'pages/hotel-form.html')
