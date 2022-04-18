7bc16f692c4afe1365104bfe26ce93c6f4b93425
shahzor khan <lamborghinia693@gmail.com>
2022-04-17 23:19:01 -0700

    main.html is complete 40%

--- restraunt/urls.py
+++ restraunt/urls.py
@@ -5,10 +5,10 @@ from . import views
 urlpatterns = [
     path('restraunt-form/', views.restrauntform, name = 'restraunt-form'),
     path('<pk>/', views.create_book, name='create-book'),
-    path('gallery', views.gallery1 , name='gallery1'),
-    path('gallery', views.gallery2 , name='gallery2'),
-    path('gallery', views.gallery3 , name='gallery3'),
-    path('gallery', views.gallery4 , name='gallery4'),
-    path('gallery', views.gallery5 , name='gallery5'),
-    path('gallery', views.gallery6 , name='gallery6'),
+    path('Ziarat', views.gallery1 , name='gallery1'),
+    path('Khuzdar', views.gallery2 , name='gallery2'),
+    path('Dalbandeen', views.gallery3 , name='gallery3'),
+    path('Peer Ghayb', views.gallery4 , name='gallery4'),
+    path('Bolan', views.gallery5 , name='gallery5'),
+    path('Gadani Beach', views.gallery6 , name='gallery6'),
 ]
