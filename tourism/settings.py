29567d3d44eb37896986cf45d2b78afadaa6e6fb
shahzoorkhan <lamborghinia693@gmail.com>
2022-04-27 17:12:05 +0500

    server side logic for restrunt form

--- tourism/settings.py
+++ tourism/settings.py
@@ -78,10 +78,43 @@ TEMPLATES = [
 ]
 
 
-JET_DEFAULT_THEME = 'light-gray'
+JET_DEFAULT_THEME = 'light-violet'
 
-JET_SIDE_MENU_COMPACT = True
+JET_SIDE_MENU_COMPACT = False
 
+
+JET_THEMES = [
+    {
+        'theme': 'default', # theme folder name
+        'color': '#47bac1', # color of the theme's button in user menu
+        'title': 'Default' # theme title
+    },
+    {
+        'theme': 'green',
+        'color': '#44b78b',
+        'title': 'Green'
+    },
+    {
+        'theme': 'light-green',
+        'color': '#2faa60',
+        'title': 'Light Green'
+    },
+    {
+        'theme': 'light-violet',
+        'color': '#a464c4',
+        'title': 'Light Violet'
+    },
+    {
+        'theme': 'light-blue',
+        'color': '#5EADDE',
+        'title': 'Light Blue'
+    },
+    {
+        'theme': 'light-gray',
+        'color': '#222',
+        'title': 'Light Gray'
+    }
+]
 
 
 WSGI_APPLICATION = 'tourism.wsgi.application'
