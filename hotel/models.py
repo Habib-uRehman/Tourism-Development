21e2b9abc963ffc367864660305fe79cb1b43337
shahzoorkhan <lamborghinia693@gmail.com>
2022-05-05 15:10:43 +0500

    display as a name in models of hotel

--- hotel/models.py
+++ hotel/models.py
@@ -10,6 +10,11 @@ from multiselectfield import MultiSelectField
 class HotelRegistration(models.Model):
 
     hotel_name = models.CharField(max_length=100) # Name of the hotel
+
+
+
+    def __str__(self):
+        return self.hotel_name
  
 
 
