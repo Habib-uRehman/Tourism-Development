de9b0371344c2700b229e380806b9758d271fc0d
shahzoorkhan <lamborghinia693@gmail.com>
2022-05-06 19:29:22 +0500

    gradually building models for hotels app just because of any misapp

--- hotel/models.py
+++ hotel/models.py
@@ -9,7 +9,14 @@ from multiselectfield import MultiSelectField
 
 class HotelRegistration(models.Model):
 
-    hotel_name = models.CharField(max_length=100) # Name of the hotel
+    hotel_name = models.CharField(max_length=100 , null=True) # Name of the hotel
+    establishment_year = models.DateField(default=datetime.date.today , null=True) # year of establishment
+    commision_date = models.DateField(default=datetime.date.today , null=True) # data of commission    
+    telex_number = models.CharField(max_length=100 , null=True) # Telex number
+    phone_number = models.CharField(max_length=100 , null=True)# Telephone Number
+    hotel_address = models.CharField(max_length=100 , null=True) # Address
+    telegraphic_address = models.CharField(max_length=100 , null=True) # Telegraphic Address
+
 
 
 
