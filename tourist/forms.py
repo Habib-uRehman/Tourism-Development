c5b994614bf3032e0fc294b0b7b29e12c2a24bb2
shahzor khan <lamborghinia693@gmail.com>
2022-05-08 20:55:13 -0700

    travel agency model is also complete

--- tourist/forms.py
+++ tourist/forms.py
@@ -2,11 +2,10 @@ from dataclasses import fields
 from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-from .models import travelagency
+from .models import 
 
 
-class Travelagencyform(ModelForm):
+class (ModelForm):
     class Meta:
-        model = travelagency
+        model = 
         fields = '__all__'
-        
--- travel/views.py
+++ travel/views.py
@@ -1,9 +1,25 @@
 import imp
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
+from .forms import Travelagencyform
 
 # Create your views here.
 
+
+    
+
+
 def travelagencyform(request):
-    return render(request, 'pages/travel-agency-form.html')    
+    
+    form = Travelagencyform()
+    if request.method == 'POST':
+        print('Printing POST' , request.POST)
+        form = Travelagencyform(request.POST)
+        if form.is_valid():
+            form.save()  
+            return redirect ('/')  
+
+            
+    context = {'form' : form}
+
+    return render(request, 'pages/travel-agency-form.html'  , context)    
